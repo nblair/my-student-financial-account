9b63cb6b194edcbe5c9ff2116ff6b106f6647938
Collin Cudd <ctcudd@wisc.edu>
2015-05-12 10:40:48 -0500

    Adding docs and a constructor to GenericDemoDataDao.  The constructor requires you to pass in a type, the type is then used by jackson to perform deserialization.

--- my-student-financial-account-impl/src/main/java/edu/wisc/student/finance/demo/DemoAuthorizedUsersDaoImpl.java
+++ my-student-financial-account-impl/src/main/java/edu/wisc/student/finance/demo/DemoAuthorizedUsersDaoImpl.java
@@ -3,60 +3,56 @@
  */
 package edu.wisc.student.finance.demo;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Named;
-
-import org.springframework.core.io.ClassPathResource;
-
-import com.fasterxml.jackson.core.type.TypeReference;
 
 import edu.wisc.student.finance.AuthorizedUserDao;
 import edu.wisc.student.finance.v1.AuthorizedUser;
 
 /**
  * Demo implementation of {@link AuthorizedUserDao}.
+ * 
  * @author Collin Cudd
  *
  */
 @Named
-public class DemoAuthorizedUsersDaoImpl extends GenericDemoDataDao<AuthorizedUser> implements AuthorizedUserDao {
+public class DemoAuthorizedUsersDaoImpl extends GenericDemoDataDao<AuthorizedUser> implements
+    AuthorizedUserDao {
+  
+  public DemoAuthorizedUsersDaoImpl() {
+    super(AuthorizedUser.class);
+  }
 
-	public static final String DEMO_AUTHZ_USERS_DATA = "edu/wisc/student/finance/demo/demo-authorized-users-service-data.json";
+  List<AuthorizedUser> demoData = new ArrayList<AuthorizedUser>();
+  public static final String DEMO_AUTHZ_USERS_DATA =
+      "edu/wisc/student/finance/demo/demo-authorized-users-service-data.json";
 
-	/* (non-Javadoc)
-	 * @see edu.wisc.student.finance.AuthorizedUserDao#getAuthorizedUsers(edu.wisc.uwss.UWUserDetails)
-	 */
-	@Override
-	public List<AuthorizedUser> getAuthorizedUsers(String pvi) {
-		return getDemoData(pvi);
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see edu.wisc.student.finance.AuthorizedUserDao#getAuthorizedUsers(edu.wisc.uwss.UWUserDetails)
+   */
+  @Override
+  public List<AuthorizedUser> getAuthorizedUsers(String pvi) {
+    return  getDemoData(pvi);
+  }
 
-	@Override
-	public void addAuthorizedUser(String pvi, AuthorizedUser authorizedUser) {
-		System.out.println(pvi + " is authorizing " + authorizedUser); //FIXME: delete or turn into sanitized log statement
-		this.data.get(pvi).add(authorizedUser);
-	}
+  @Override
+  public void addAuthorizedUser(String pvi, AuthorizedUser authorizedUser) {
+    System.out.println(pvi + " is authorizing " + authorizedUser); // FIXME: delete or turn into
+                                                                   // sanitized log statement
+    this.data.get(pvi).add(authorizedUser);
+  }
 
-	/* (non-Javadoc)
-	 * @see edu.wisc.student.finance.demo.GenericDemoDataDao#getDemoDataPath()
-	 */
-	@Override
-	public String getDemoDataPath() {
-		return DEMO_AUTHZ_USERS_DATA;
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.wisc.student.finance.demo.GenericDemoDataDao#readDemoData()
-	 */
-	@Override
-	protected Map<String, List<AuthorizedUser>> readDemoData() throws IOException {
-		try (InputStream is = new ClassPathResource(getDemoDataPath()).getInputStream()) {
-			return mapper.readValue(is, new TypeReference<Map<String, List<AuthorizedUser>>>() { });
-		}
-	}
-
+  /*
+   * (non-Javadoc)
+   * 
+   * @see edu.wisc.student.finance.demo.GenericDemoDataDao#getDemoDataPath()
+   */
+  @Override
+  public String getDemoDataPath() {
+    return DEMO_AUTHZ_USERS_DATA;
+  }
 }
--- my-student-financial-account-impl/src/main/java/edu/wisc/student/finance/demo/DemoChargeDaoImpl.java
+++ my-student-financial-account-impl/src/main/java/edu/wisc/student/finance/demo/DemoChargeDaoImpl.java
@@ -3,30 +3,29 @@
  */
 package edu.wisc.student.finance.demo;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 import javax.inject.Named;
-
-import org.springframework.core.io.ClassPathResource;
-
-import com.fasterxml.jackson.core.type.TypeReference;
 
 import edu.wisc.student.finance.ChargeDao;
 import edu.wisc.student.finance.v1.ChargeType;
 
 /**
- * Demo implementation of {@link ChargeDao}.
+ * Demo implementation of {@link ChargeDao} backed by {@link GenericDemoDataDao}.
  * 
  * @author Nicholas Blair
  */
 @Named
 public class DemoChargeDaoImpl extends GenericDemoDataDao<ChargeType> implements ChargeDao {
 	
-	public static final String DEMO_CHARGE_DATA = "edu/wisc/student/finance/demo/demo-charge-service-data.json";
+	/**
+   * @param typeParameterClass
+   */
+  public DemoChargeDaoImpl() {
+    super(ChargeType.class);
+  }
+
+  public static final String DEMO_CHARGE_DATA = "edu/wisc/student/finance/demo/demo-charge-service-data.json";
   
   /*
    * (non-Javadoc)
@@ -44,14 +43,4 @@ public class DemoChargeDaoImpl extends GenericDemoDataDao<ChargeType> implements
 	public String getDemoDataPath() {
 		return DEMO_CHARGE_DATA;
 	}
-
-	/* (non-Javadoc)
-	 * @see edu.wisc.student.finance.demo.GenericDemoDataDao#readDemoData()
-	 */
-	@Override
-	protected Map<String, List<ChargeType>> readDemoData() throws IOException {
-	   try (InputStream is = new ClassPathResource(getDemoDataPath()).getInputStream()) {
-		       return mapper.readValue(is, new TypeReference<Map<String, List<ChargeType>>>() { });
-		   }
-	   }
 }
